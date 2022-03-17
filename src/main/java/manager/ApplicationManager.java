a70643ed0fd3eb8d7b86c8660b6a09df3930dd8d
TayaHatum <21taya21@gmail.com>
2022-03-17 20:39:10 +0200

    add manager

---
+++ src/main/java/manager/ApplicationManager.java
@@ -0,0 +1,32 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+
+    WebDriver wd;
+    UserHelper userHelper;
+
+
+    public void init(){
+        wd=new ChromeDriver();
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wd.navigate().to("https://ilcarro.xyz/");
+        userHelper = new UserHelper(wd);
+
+
+
+    }
+
+    public void stop(){
+       // wd.quit();
+    }
+
+    public UserHelper user() {
+        return userHelper;
+    }
+}
---
+++ src/main/java/manager/UserHelper.java
@@ -0,0 +1,9 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+
+public class UserHelper extends HelperBase{
+    public UserHelper(WebDriver wd) {
+        super(wd);
+    }
+}
