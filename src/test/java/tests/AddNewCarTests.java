0df9915fa6915883dcb8827432a84661717aefc1
TayaHatum <21taya21@gmail.com>
2022-03-25 10:26:56 +0300

    added car

---
+++ src/test/java/tests/AddNewCarTests.java
@@ -0,0 +1,33 @@
+package tests;
+
+import models.Car;
+import org.testng.annotations.Test;
+
+public class AddNewCarTests extends TestBase{
+
+
+    @Test
+    public void addNewCarSuccess(){
+        int index = (int) (System.currentTimeMillis() / 1000) % 36000;
+
+        Car car = Car.builder()
+                .address("Tel Aviv, Israel")
+                .make("BMW")
+                .model("M5")
+                .year("2022")
+                .engine("2.5")
+                .fuel("Petrol")
+                .gear("MT")
+                .wD("AWD")
+                .doors("5")
+                .seats("4")
+                .clasS("C")
+                .fuelConsumption("6.5")
+                .carRegNumber("100-22-"+index)
+                .price("65")
+                .distanceIncluded("500")
+                .features("Type of features")
+                .about("Very good car")
+                .build();
+    }
+}
